6d30046c6cc7d2fe53c64002e03a40aeebd4dea6
VighneshS <vighneshsivaraman@gmail.com>
2021-07-01 16:34:24 -0500

    Quiz 5 Q5(2)

--- static/scripts.js
+++ static/scripts.js
@@ -123,9 +123,6 @@ function indexData() {
 }
 
 function getStoredData() {
-    spinner.show()
-    searchResults.hide()
-    indexingResults.hide()
     let payLoad = {
         'type': 'stored_data'
     }
@@ -141,8 +138,6 @@ function getStoredData() {
         for (const dataKey in data) {
             stored_data.append($('<div>' + data[dataKey]['documentName'] + ': ' + data[dataKey]['wordCount'] + '<div>'))
         }
-        spinner.hide()
-        indexingResults.show()
     });
 }
 
